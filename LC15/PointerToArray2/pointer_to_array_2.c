eeced72d85923679bc1f6da59434de684c455178
Vignesh Arvind Rameshwar <vignesh.rameshwar@gmail.com>
2023-10-23 14:29:35 +0100

    First commit

--- LC15/PointerToArray2/pointer_to_array_2.c
+++ LC15/PointerToArray2/pointer_to_array_2.c
@@ -21,11 +21,11 @@ int main(void) // Main : Execution starts here...
 
     printf ("Pointer method 2 using index\n");
     for  ( i = 0 ; i < 10 ; i++ )
-        printf ("Item %d contains value %d\n", i, *(pArray+i) );
+        printf ("Item %d contains value %d\n", i, *(pArray+i) ); // Initial pointer of pArray + i
 
     printf ("Pointer method 2 (only works for moving sequentially\n");
     for  ( i = 0 ; i < 10 ; i++ )
-        printf ("Item %d contains value %d\n", i, *pArray++ );
+        printf ("Item %d contains value %d\n", i, *pArray++ );  // Actually change the pointer of pARRAY
 
     // Exit the application
     return 0;
